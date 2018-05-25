cd480809dd487f8849f00fa302e7104521520920
VisonYH <2065610194@qq.com>
2018-05-25 14:43:28 +0800

    完成简单的综合和下载功能

---
+++ src/common/config/config.js
@@ -0,0 +1,6 @@
+export const featureType = {
+  Building: ['Accommodation', 'Commercial', 'Religious', 'Civic/Amenity', 'Other Buildings'],
+  Highway: ['Roads', 'Link roads', 'Special road types'],
+  Railway: ['Tracks', 'Additional features', 'Stations and Stops', 'Other railways'],
+  Amenity: ['Sustenance', 'Education', 'Transportation', 'Financial', 'Healthcare', 'Entertainment, Arts & Culture']
+}
